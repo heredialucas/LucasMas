e3804df491588c53f38178d8bb1bee285834492a
heredialucas <heredialucasfac22@hotmail.com>
2022-09-30 22:03:49 -0300

    mejorando portafolio

--- src/data.js
+++ src/data.js
@@ -1,27 +1,27 @@
-import cari1 from './assets/images/caricatura1.jpeg';
-import cari2 from './assets/images/caricatura2.jpeg';
-import cari3 from './assets/images/caricatura3.jpeg';
-import cari4 from './assets/images/caricatura4.jpeg';
-import cari5 from './assets/images/caricatura5.jpeg';
-import casa1 from './assets/images/casa1.jpeg';
-import casa2 from './assets/images/casa2.jpeg';
-import casa3 from './assets/images/casa3.jpeg';
-import casa4 from './assets/images/casa4.jpeg';
-import casa5 from './assets/images/casa5.jpeg';
-import casa6 from './assets/images/casa6.jpeg';
-import casa7 from './assets/images/casa7.jpeg';
-import casa8 from './assets/images/casa8.jpeg';
-import casa9 from './assets/images/casa9.jpeg';
-import casa10 from './assets/images/casa10.jpeg';
-import casa11 from './assets/images/casa11.jpeg';
-import casa12 from './assets/images/casa12.jpeg';
-import casa13 from './assets/images/casa13.jpeg';
-import casa14 from './assets/images/casa14.jpeg';
-import casa15 from './assets/images/casa15.jpeg';
-import edificio1 from './assets/images/edificio1.jpeg';
-import edificio2 from './assets/images/edificio2.png';
-import edificio3 from './assets/images/edificio3.jpeg';
- 
+import cari1 from "./assets/images/caricatura1.jpeg";
+import cari2 from "./assets/images/caricatura2.jpeg";
+import cari3 from "./assets/images/caricatura3.jpeg";
+import cari4 from "./assets/images/caricatura4.jpeg";
+import cari5 from "./assets/images/caricatura5.jpeg";
+import casa1 from "./assets/images/casa1.jpeg";
+import casa2 from "./assets/images/casa2.jpeg";
+import casa3 from "./assets/images/casa3.jpeg";
+import casa4 from "./assets/images/casa4.jpeg";
+import casa5 from "./assets/images/casa5.jpeg";
+import casa6 from "./assets/images/casa6.jpeg";
+import casa7 from "./assets/images/casa7.jpeg";
+import casa8 from "./assets/images/casa8.jpeg";
+import casa9 from "./assets/images/casa9.jpeg";
+import casa10 from "./assets/images/casa10.jpeg";
+import casa11 from "./assets/images/casa11.jpeg";
+import casa12 from "./assets/images/casa12.jpeg";
+import casa13 from "./assets/images/casa13.jpeg";
+import casa14 from "./assets/images/casa14.jpeg";
+import casa15 from "./assets/images/casa15.jpeg";
+import edificio1 from "./assets/images/edificio1.jpeg";
+import edificio2 from "./assets/images/edificio2.png";
+import edificio3 from "./assets/images/edificio3.jpeg";
+
 export const CaricaturasData = [
   {
     id: 1,
@@ -47,7 +47,7 @@ export const CaricaturasData = [
     id: 5,
     title: "cari5",
     img: cari5,
-  }
+  },
 ];
 export const CasasData = [
   {
@@ -142,66 +142,6 @@ export const DiseñosData = [
     title: "casa3",
     img: casa3,
   },
-  {
-    id: 4,
-    title: "casa4",
-    img: casa4,
-  },
-  {
-    id: 5,
-    title: "casa5",
-    img: casa5,
-  },
-  {
-    id: 6,
-    title: "casa6",
-    img: casa6,
-  },
-  {
-    id: 7,
-    title: "casa7",
-    img: casa7,
-  },
-  {
-    id: 8,
-    title: "casa8",
-    img: casa8,
-  },
-  {
-    id: 9,
-    title: "casa9",
-    img: casa9,
-  },
-  {
-    id: 10,
-    title: "casa10",
-    img: casa10,
-  },
-  {
-    id: 11,
-    title: "casa11",
-    img: casa11,
-  },
-  {
-    id: 12,
-    title: "casa12",
-    img: casa12,
-  },
-  {
-    id: 13,
-    title: "casa13",
-    img: casa13,
-  },
-  {
-    id: 14,
-    title: "casa14",
-    img: casa14,
-  },
-  {
-    id: 15,
-    title: "casa15",
-    img: casa15,
-  },
 ];
 export const EdificiosData = [
   {
@@ -218,7 +158,5 @@ export const EdificiosData = [
     id: 3,
     title: "edificio3",
     img: edificio3,
-  }
+  },
 ];
-
- 
